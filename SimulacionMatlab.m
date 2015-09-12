ad65757f32e4196f5d85d8b5464af4f7f9e6a2da
flopezmontero <falm81427@gmail.com>
2015-09-12 12:14:24 -0600

    Update SimulacionMatlab.m

--- SimulacionMatlab.m
+++ SimulacionMatlab.m
@@ -3,10 +3,16 @@
 %Prof. Dr.-Ing. Alfonso Chac?n Rodr?guez
 %Estudiantes: Francis L?pez Montero /Jeffry Quir?s Fallas
 
-%Par?metros MOSFET
+%Par?metros NMOST
 
-wn=3.0e-6;
-ln=0.6e-6;
+lambda=0.3*10^-6;
+wn=12*lambda;
+ln=2*lambda;
+
+%Parámetros PMOS
+x=1;
+lp=ln;
+wp=x*wn;
 %Voltaje Umbral N 
 Vtn=0.77;
 
@@ -16,51 +22,54 @@ Vtp=(-0.89);
 %Tension alimentacion
 Vdd=3.3;
 
-tox=1.41e-8;
+tox=1.41;
 e0=8.854e-12;%Permitividad del vac?o
-Cox=3.6*e0/tox;
+Cox=3.6*e0/(tox*10^-9);
 
 %Voltaje cr?tico
-vsatn=10e7; %velocidad saturacion electrones
+vsatn=10^7; %velocidad saturacion electrones
 vsatp=8e6; %velocidad saturacion huecos
 n=1;
 
-r=2;%Proporcion beta PMOS respecto a NMOS
+r=3;%Proporcion beta PMOS respecto a NMOS
 
-for Vin=0:0.3:Vdd
+for Vin=0:0.5:Vdd
     Vingraf(n)=Vin;
     m=1;
+    %Par?metros PMOS
+    up=(185)/(1+(abs(Vin-1.5*Vtp)/(0.338 * tox)));%Efectividad de canal
+    Vcp=lp*(2*vsatp/up);
+    VGTp=Vin + Vtp;
+    Vdpsat=(VGTp*Vcp)/(VGTp+Vcp);
+    
+    %Parametros NMOS
+    un=(540)/(1+((Vin+Vtn)/(0.54 * tox))^1.85);
+    Vcn=ln*(2*vsatn/un);
+    VGTn=Vin-Vtn;
+    Vdnsat=(VGTn*Vcn)/(VGTn+Vcn);
+    
     for Vout=0:0.3:Vdd
         Voutgraf(m)=Vout;
- 
-        %Par?metros PMOS
-        up=(185)/(1+(abs(Vin+1.5*Vtp)/(0.338e9 * tox)));%Efectividad de canal
-        Vcp=ln*(2*vsatp/up);
-        VGTp=Vin+Vtp;
-        Vdpsat=(VGTp*Vcp)/(VGTp+Vcp);
+     
 
         %Ecuaciones region PMOS
-        if(Vin>Vtp + Vdd)
-            Idsp(m,n)=0;
-        elseif((Vin<Vtp + Vdd) && (Vout>Vdpsat))
+        if(Vin>Vdd + Vtp)
+            Idp=0;
+        elseif((Vin<Vdd + Vtp) && (Vout>Vdpsat))
             Vds=Vdd-Vout;
-            Idsp(m,n)=(up/(1+(Vds/Vcp)))*Cox*r*(wn/ln)*(VGTp-(Vds/2))*Vds;
-        elseif((Vin<Vtp + Vdd) && (Vout<=Vdpsat))
-            Idsp(m,n)=Cox*r*wn*(VGTp-Vdpsat)*vsatp;
+            Idp=(up/(1+(Vds/Vcp)))*Cox*r*(wp/lp)*(VGTp-(Vds/2))*Vds;
+        elseif((Vin<Vdd + Vtp) && (Vout<=Vdpsat))
+            Idp=Cox*r*wp*(VGTp-Vdpsat)*vsatp;
         end
+        Idsp(m,n)=abs(Idp);
 %_-------------------------------------------------------------------------        
-        %Parametros NMOS
-        un=(540)/(1+((Vin+Vtn)/(0.54e9 * tox))^1.85);
-        Vcn=ln*(2*vsatn/un);
-        VGTn=Vin-Vtn;
-        Vdnsat=(VGTn*Vcn)/(VGTn+Vcn);
-
+        %Ecuaciones region NMOS
         if(Vin<Vtn)
             Idsn(m,n)=0;
-        elseif((Vin>Vtn)&&(Vout<Vdpsat))
+        elseif((Vin>Vtn)&&(Vout<Vdnsat))
             Vds=Vout;
             Idsn(m,n)=(un/(1+(Vds/Vcn)))*Cox*(wn/ln)*(VGTn-(Vds/2))*Vds;
-        elseif((Vin>Vtn)&&(Vout>=Vdpsat))
+        elseif((Vin>Vtn)&&(Vout>=Vdnsat))
             Idsn(m,n)=Cox*wn*(VGTn-Vdnsat)*vsatn;
         end
         m=m+1;
@@ -68,7 +77,7 @@ for Vin=0:0.3:Vdd
     n=n+1;
 end
 
-plot(Vingraf,Idsn,'b',Vingraf,Idsp,'r')
+plot(Voutgraf,Idsn,'b',Voutgraf,Idsp,'r')
 xlabel('Vds(V)')
 ylabel('Ids(A)')
 axis on
