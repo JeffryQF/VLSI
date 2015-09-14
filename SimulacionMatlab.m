53749cf2339bf9caa97f05f37ce3ec632504c2bd
flopezmontero <falm81427@gmail.com>
2015-09-14 16:36:43 -0600

    cambio final

--- SimulacionMatlab.m
+++ SimulacionMatlab.m
@@ -10,7 +10,7 @@ wn=12*lambda;
 ln=2*lambda;
 
 %Par?metros PMOS
-x=2.418;
+x=4;
 lp=ln;
 wp=x*wn;
 %Voltaje Umbral N 
@@ -35,24 +35,28 @@ Vinv=(Vdd+Vtp+Vtn*sqrt(1/r))/(1+sqrt(1/r));
 
 n=1;
 
-for Vin=0:0.5:Vdd
+for Vin=0:0.1:Vdd
     Vingraf(n)=Vin;
     m=1;
     VGTp=abs(Vin-Vdd) + Vtp;    
     VGTn=Vin-Vtn;
-    
-    for Vout=0:0.3:Vdd
+    cp=0;
+    cn=0;
+    for Vout=0:0.1:Vdd
         Voutgraf(m)=Vout;
      
 
         %Ecuaciones region PMOS
-        if(Vin>Vdd + Vtp)
+        if(Vin>Vdd + Vtp)%PMOS CORTE
             Idp=0;
-        elseif((Vin<Vdd + Vtp) && (Vout>Vin - Vtp))
+            
+        elseif((Vin<Vdd + Vtp) && (Vout>Vin - Vtp))%PMOS LINEAL
             Vdsp=abs(Vout-Vdd);
             Idp=betap*(wp/lp)*(2*VGTp-Vdsp)*Vdsp;
-        elseif((Vin<Vdd + Vtp) && (Vout<Vin - Vtp))
+            
+        elseif((Vin<Vdd + Vtp) && (Vout<Vin - Vtp))%PMOS SATURACION
             Idp=betap*(wp/lp)*(VGTp)^2;
+            
         end
         Idsp(m,n)=abs(Idp);
 %_-------------------------------------------------------------------------        
@@ -67,12 +71,40 @@ for Vin=0:0.5:Vdd
         end
         m=m+1;
     end
+    if(Vin<Vtn)
+        Vo(n)=Vdd;
+    elseif(Vtn<=Vin && Vin<Vinv)
+        a=1;
+        b=(-2*VGTp);
+        c=((VGTn)^2)/r;
+        d=(b^2)-4*a*c;
+        x1=(-b-sqrt(d))/(2*a);
+        Vo(n)= 3.3-x1;
+    elseif(Vin==Vinv)
+        Vo(n)=Vinv;
+    elseif(Vinv<Vin && Vin<=Vdd+Vtp)
+        a=1;
+        b=(-2*VGTn);
+        c=((VGTp)^2)*r;
+        d=(b^2)-4*a*c;
+        x1=(-b-sqrt(d))/(2*a);
+        Vo(n)= x1;
+    elseif (Vin>Vdd+Vtp)
+        Vo(n)=0;    
+    end
     n=n+1;
 end
-
+subplot(1,2,1);
 plot(Voutgraf,Idsn,'b',Voutgraf,Idsp,'r')
 xlabel('Vds(V)')
 ylabel('Ids(A)')
+axis on
+grid on
+box off
+subplot(1,2,2);
+plot(Vingraf,Vo,'g',Vingraf,Vingraf,'y')
+xlabel('Vin(V)')
+ylabel('Vout(V)')
 axis on
 grid on
 box off
